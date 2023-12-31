aa3737640788add73ca0519e500ab6568a1e85a7
rishabhknowss <rishabhrai1515@gmail.com>
2023-12-31 19:58:27 +0530

    created script for default dockerfile

---
+++ autoDock.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const cli = require('cli');
+
+cli.parse({
+    filename: ['f', 'Name of the Dockerfile to create', 'string', 'Dockerfile'],
+    baseImage: ['b', 'Base image for the Dockerfile', 'string', 'node:14'],
+    appDirectory: ['d', 'Directory containing your app files', 'string', 'app'],
+});
+
+cli.main(function (args, options) {
+    const filename = options.filename;
+    const baseImage = options.baseImage;
+    const appDirectory = options.appDirectory;
+
+    const dockerfileContent = `# Use the specified base image
+FROM ${baseImage}
+
+# Set the working directory
+WORKDIR /${appDirectory}
+
+# Copy the app files to the container
+COPY . .
+
+# Specify the default command to run when the container starts
+CMD ["npm", "start"]`;
+
+    fs.writeFileSync(filename, dockerfileContent, 'utf-8');
+    console.log(`Dockerfile '${filename}' created successfully.`);
+});
