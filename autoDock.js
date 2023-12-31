5aa03af555933960ef833668cb0ef7148e592bb9
rishabhknowss <rishabhrai1515@gmail.com>
2024-01-01 01:09:43 +0530

    fixed bugs

--- autoDock.js
+++ autoDock.js
@@ -9,7 +9,7 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 async function generateDockerCompose(baseImages) {
     // For text-only input, use the gemini-pro model
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-    const prompt = `you are writing a Docker Compose file adhering to industry standards. Define services based on the following images: ${baseImages.join(', ')}. Utilize default configurations and incorporate essential specifications such as volumes, ports, networks, health checks, and more for each image and service. Ensure strict adherence to the given images and services, and avoid including any additional services or images beyond the specified ones.`;
+    const prompt = `you are writing a Docker Compose file adhering to industry standards. Define services based on the following images: ${baseImages.join(', ')}. Utilize default configurations and incorporate essential specifications such as volumes, ports, networks, health checks, and more for each image and service. Ensure strict adherence to the given images and services, and avoid including any additional services or images beyond the specified ones. dont write comments `;
 
 
     const result = await model.generateContent(prompt);
@@ -26,8 +26,7 @@ cli.parse({
 cli.main(async function (args, options) {
     const filename = options.filename;
     const baseImages = Array.isArray(options.baseImage) ? options.baseImage : [options.baseImage];
-    console.log(baseImages.join(', '))
-
+   
 
     if (baseImages.length === 0) {
         cli.error('Please provide at least one base image using the -b option.');
@@ -37,6 +36,7 @@ cli.main(async function (args, options) {
 
     try {
         const dockerComposeContent = await generateDockerCompose(baseImages);
+        console.log(dockerComposeContent)
         fs.writeFileSync(filename, dockerComposeContent, 'utf-8');
         cli.ok(`Docker Compose file '${filename}' created successfully.`);
     } catch (error) {
